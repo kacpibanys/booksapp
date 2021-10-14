23f732168b251819da4f6436de68d10622fc25dd
kacpibanys <kacpibanys@gmail.com>
2021-10-14 20:19:59 +0200

    event delegation explanation

--- src/js/script.js
+++ src/js/script.js
@@ -5,49 +5,62 @@
     templateOf: {
       bookTemplate: '#template-book',
     },
-
     booksPanel: {
       bookList: '.books-list',
-    }
+    },
+
+  };
+
+  const classNames = {
+    book: {
+      hiddenBook: 'hidden',
+      favorite: 'favorite',
+    },
   };
 
   const templates = {
     menuBook: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
   };
+  var favouriteBooks = [];
 
   const app = {
 
     render: function(){
-      const thisApp = this;
-      //console.log('menuBook:', templates);
+      //const thisApp = this;
 
+      const bookContainer = document.querySelector(select.booksPanel.bookList);
+      for(let book of dataSource.books){
 
-      // for(let book of dataSource.books) {
-      //   thisApp.generatedDOM = '';
-      //   const genereatedHTML = templates.menuBook(thisApp);
-      //   thisApp.generatedDOM = utils.createDOMFromHTML(genereatedHTML);
+        const generatedHTML = templates.menuBook(book);
+        const dom = utils.createDOMFromHTML(generatedHTML);
 
-
-      // }
-
-      // for( let i = 0; i < dataSourceLength; i++ ){
-      //   const genereatedHTML = templates.menuBook(thisApp);
-      //   thisApp.select.booksPanel.bookList = utils.createDOMFromHTML(genereatedHTML);
-      //   const bookContainer = document.querySelector(select.booksPanel.bookList);
-      //   bookContainer.appendChild(thisApp.booksPanel.bookList);
-      //   console.log('Loop iteration: ' + i);
-      // }
-
-
-
-      for(let book in thisApp.dataSource.books){
-        const generatedHTML = templates.menuBook(thisApp);
-        book = utils.createDOMFromHTML(generatedHTML);
-        const bookContainer = document.querySelector(select.booksPanel.bookList);
-        bookContainer.appendChild(book);
+        bookContainer.appendChild(dom);
       }
-      // console.log('test', dataSourceLength);
     },
+    initActions: function(){
+      const bookImageReferences = document.querySelectorAll('.book__image');
+      console.log('bookReferences', bookImageReferences);
+      for (let image of bookImageReferences) {
+        image.addEventListener('dblclick', function(event){
+          event.preventDefault();
+          const bookID = image.getAttribute('data-id');
+          if (!favouriteBooks.includes(bookID)) {
+            image.classList.add(classNames.book.favorite);
+            favouriteBooks.push(bookID);
+            console.log('favourtieBooks', favouriteBooks);
+          } else {
+            image.classList.remove(classNames.book.favorite);
+            favouriteBooks.pop(bookID);
+            console.log('favourtieBooks', favouriteBooks);
+          }
+
+
+          // console.log('bookID', bookID);
+          // console.log('favourtieBooks', favouriteBooks);
+        });
+      }
+    }
   };
   app.render();
+  app.initActions();
 }
